5321ebc800121c5b208762dcf39d3aa1df756818
unknown <ibr.s.ahmat@gmail.com>
2022-11-26 23:37:54 +0300

    Create UserModel & install bcrypt

---
+++ src/models/userModel.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+export const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  hashPassword: {
+    type: String,
+    required: true,
+  },
+  created_date: {
+    type: Date,
+    default: Date.now,
+  },
+});
